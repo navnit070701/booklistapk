f9e9b384431413f537718761eece6942de9f4179
navnit070701 <navnit0707@gmail.com>
2021-05-18 23:37:47 +0530

    done

--- server/app.js
+++ server/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
+// allow cross-origin requests
+app.use(cors());
+
+// connect to mlab database
+
+mongoose.connect('mongodb+srv://navnit:test@cluster0.flxcq.mongodb.net/gql-navnit?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
 
 
 app.use('/graphql',graphqlHTTP({
